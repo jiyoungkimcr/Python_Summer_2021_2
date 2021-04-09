398067f390c95f0f5975da6f7cab8f87166211c9
Jiyoung Kim <jiyoungkimcr@gmail.com>
2021-04-09 14:01:41 +0200

    Blocks and variable visibility

--- string_vars.py
+++ string_vars.py
@@ -10,7 +10,25 @@ print(s[-3:])
 s2 = 'abc\n\rdef'
 print(s2)
 
-#\r???
-
-
 print(ord('a'))
+
+#\r???
+s3 = 'gsgaloozzca'
+print(s3)
+
+s4 = 'abc\tdef\t\t\tegwg'
+print(s4)
+
+# How to escape the characters?
+s5 = 'abc\td\ef\t\t\te\gwg'
+print(s5)
+s6 = "agsag'agsg"
+s6 = 'agsag\'agsg'
+print(s6)
+
+s7 = '          asgsgsag  asggs   '
+print(s7)
+print(s7.strip()) #this get rids of the spaces
+
+s5_tab = s5.split('\t')
+print(s5_tab)
